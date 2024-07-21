cb32b832fbd5173a0b3f3dc1eb577a5ae1162801
shakeeb2001 <ahamedshakeeb123@gmail.com>
2024-07-21 12:17:36 +0530

    git_commit

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const itemRoutes = require('./routes/itemRoutes');
+// require('dotenv').config();
+
+// const app = express();
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // Routes
+// app.use('/api', itemRoutes);
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+
+// })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.log('MongoDB connection error:', err));
+
+// // Global error handler
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
+
+// // Start the server
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -18,10 +53,12 @@ app.use('/api', itemRoutes);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-
 })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit the process if there's a MongoDB connection error
+  });
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -32,3 +69,5 @@ app.use((err, req, res, next) => {
 // Start the server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
